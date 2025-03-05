f4be260597d53f69d1d70a46a0fb1315d076c586
Tarek <saadfouad1976t@gmail.com>
2025-03-05 18:53:54 +0200

    make all routes work on flask ( concept order , learning object , random learning object )

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ from src.controllers.http.GetLOsController import LO_controller
 from src.controllers.http.random_losController import concept_controller
 
 app = Flask(__name__)
-app.register_blueprint(graph_controller)
+
+# Register all Blueprints
+app.register_blueprint(graph_controller)  # For concept order related routes
+app.register_blueprint(LO_controller)  # For learning object related routes
+app.register_blueprint(concept_controller)  # For random learning object related routes
 
 if __name__ == "__main__":
     app.run(debug=True)
