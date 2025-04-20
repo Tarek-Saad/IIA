7788b4f23a20ceaf146d4e4eddab835e21ff64e9
Tarek <saadfouad1976t@gmail.com>
2025-04-20 14:09:48 +0200

    CORS : Add learning analysis controller with POST endpoint for analyzing user knowledge

--- src/controllers/http/generate_learning_analysisController.py
+++ src/controllers/http/generate_learning_analysisController.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, request, jsonify
+from flask_cors import CORS
 from src.core.services.generate_learning_analysis import generate_learning_analysis
 
 learning_analysis_controller = Blueprint('learning_analysis_controller', __name__)
+CORS(learning_analysis_controller)
 
 @learning_analysis_controller.route('/analyze', methods=['POST'])
 def analyze_learning():
